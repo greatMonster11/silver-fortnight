cda428a9b5b31b824ecf6dd32283d0986382bf6e
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2019-12-12 21:08:32 +0700

    add - components

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react";
+import "./App.css";
+import Header from "./Header";
+import Movie from "./Movie";
+import Search from "./Search";
 
 function App() {
   return (
